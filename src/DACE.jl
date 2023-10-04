d24b6cb8175e04e3c10604bc762dc5360ada5c61
Chris Scott <chris@chrisdjscott.co.uk>
2023-10-05 08:45:58 +1300

    add function that returns dace library version

--- src/DACE.jl
+++ src/DACE.jl
@@ -1,5 +1,15 @@
 module DACE
 
-greet() = print("Hello World!")
+using DACE_jll
+
+function dacelibversion()
+    # void daceGetVersion(int *imaj, int *imin, int *ipat)
+    imaj = Ref(Cint(0))
+    imin = Ref(Cint(0))
+    ipat = Ref(Cint(0))
+    ccall((:daceGetVersion, libdace), Cvoid, (Ref{Cint}, Ref{Cint}, Ref{Cint}), imaj, imin, ipat)
+
+    return imaj[], imin[], ipat[]
+end
 
 end # module DACE
