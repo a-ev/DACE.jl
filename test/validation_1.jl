802adb843cb35da556a73777146ea5defbad8ec5
Chris Scott <chris@chrisdjscott.co.uk>
2024-01-10 13:49:52 +1300

    finish validation_1

--- test/validation_1.jl
+++ test/validation_1.jl
@@ -11,4 +11,27 @@
             end
         end
     end
+
+    @testset "1.2 Truncation order" begin
+        for k_user = 1:10
+            @testset "1.2 Trunction order (k_user=$k_user)" begin
+                DACE.init(10, 6)
+
+                DACE.setTO(k_user)
+                k_dace = DACE.getTO()
+                @test k_dace == k_user
+            end
+        end
+    end
+
+    @testset "1.3 Cutoff for the coefficients" begin
+        for cutoff_user in (1e-14, 1e-15, 1e-16)
+            @testset "1.3 Cutoff for the coefficients (cutoff_user=$cutoff_user)" begin
+                DACE.init(10, 6)
+                DACE.setEps(cutoff_user)
+                cutoff_dace = DACE.getEps()
+                @test cutoff_dace == cutoff_user
+            end
+        end
+    end
 end
