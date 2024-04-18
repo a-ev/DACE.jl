125e3979ebc477c1f1d05339154dc814d9dfa53d
Chris Scott <chris@chrisdjscott.co.uk>
2024-04-18 13:30:14 +1200

    add new tutorial to navigation

--- docs/make.jl
+++ docs/make.jl
@@ -34,6 +34,7 @@ example_pages = [
 tutorial_pages = [
     "Setting up your development environment" => "tutorials/setting-up-your-development-environment.md",
     "Modifying the C++ side of the interface" => "tutorials/modifying-the-cxx-side-of-the-interface.md",
+    "Making a new release of DACE_jll" => "tutorials/making-a-new-release-of-dace_jll.md",
 ]
 
 pages = [
